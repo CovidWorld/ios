acdd89af973d6ac7addf4ea0ec9bc18c9160a536
Boris Kolozsi <boris.kolozsi@gmail.com>
2020-05-04 23:50:06 +0200

    updated symptoms page

--- Covid/AdditionalInfo/SymptomsViewController.swift
+++ Covid/AdditionalInfo/SymptomsViewController.swift
@@ -27,8 +27,8 @@ final class SymptomsViewController: ViewController {
 
     @IBOutlet private weak var bodyLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        super.loadView()
 
         updateUI()
     }
@@ -37,27 +37,11 @@ final class SymptomsViewController: ViewController {
 extension SymptomsViewController {
 
     private func updateUI() {
-        let bullet = "•  "
-        var strings = [String]()
-        strings.append("suchým, zadúšavým, dráždivým kašľom bez hlienov")
-        strings.append("bolestami hlavy, kĺbov a svalov")
-        strings.append("celkovou vyčerpanosťou")
-        strings.append("pocitom ťažoby a pichaním na hrudníku alebo pocitom tzv. “nedostatočného nádychu”.")
-        strings.append("""
-                vysoká teplota na hranici, alebo tesne za hranicou 38 stupňov, ale rovnako aj vysoko za hranicou 38 stupňov, \
-                v kombinácii s kašľom a zvyškom uvedených príznakov sú taktiež príznakom ochorenia.
-                """)
-        strings = strings.map { bullet + $0 }
-
-        var attributes = [NSAttributedString.Key: Any]()
-        attributes[.font] = UIFont(name: "Poppins-Regular", size: 15)
-        attributes[.foregroundColor] = UIColor(red: 57 / 255, green: 57 / 255, blue: 57 / 255, alpha: 1)
-
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.headIndent = (bullet as NSString).size(withAttributes: attributes).width
-        attributes[.paragraphStyle] = paragraphStyle
-
-        let string = strings.joined(separator: "\n\n")
-        bodyLabel.attributedText = NSAttributedString(string: string, attributes: attributes)
+        let text = "Ľudia nakazení COVID-19 udávajú široké spektrum prejavov, od miernych až po závažné.\n\nPrejavy sa zvyčajne objavujú 2-14 dní po vystavení sa nákaze a môžu zahŕňať:"
+        let attributes: [NSAttributedString.Key: Any] = [.font: UIFont(name: "Poppins-Light", size: 17.0)!, .foregroundColor: UIColor.darkGray]
+        let attributedString = NSMutableAttributedString(string: text, attributes: attributes)
+        let boldRange = (attributedString.string as NSString).range(of: "2-14 dní po vystavení sa nákaze")
+        attributedString.setAttributes([.font: UIFont(name: "Poppins-Bold", size: 17.0)!], range: boldRange)
+        bodyLabel.attributedText = attributedString
     }
 }
